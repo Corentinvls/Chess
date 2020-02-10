fa585d65541ca7ff83ccd1b0101f9b1bb15617d4
vallois <corentin.vallois@edu.itescia.fr>
2020-02-10 10:02:39 +0100

    import

--- src/main/java/fr/rphstudio/chess/game/ChessModel.java
+++ src/main/java/fr/rphstudio/chess/game/ChessModel.java
@@ -1,4 +1,17 @@
 package fr.rphstudio.chess.game;
 
+import fr.rphstudio.chess.interf.IChess;
+
 public class ChessModel {
+
+
+    private static IChess IChess;
+
+    public ChessModel() {
+
+    }
+
+    public static IChess getInstance() {
+        return IChess;
+    }
 }
