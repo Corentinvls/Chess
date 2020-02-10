884e60a4e10d5ebd0b14debbc3f94dc5fee7d990
vallois <corentin.vallois@edu.itescia.fr>
2020-02-10 14:43:56 +0100

    piece placée

--- src/main/java/fr/rphstudio/chess/game/GameBoard.java
+++ src/main/java/fr/rphstudio/chess/game/GameBoard.java
@@ -6,42 +6,73 @@ import java.util.Arrays;
 
 public class GameBoard {
 
-private Piece[][]gameBoard ;
+    private Piece[][] gameBoard;
 
     public GameBoard() {
         this.gameBoard = new Piece[8][8];
 
-        for (int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                gameBoard[i][j]=null;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                gameBoard[i][j] = null;
             }
         }
-       initPieces();
+        initPieces();
 
     }
 
     private void initPieces() {
-        for (int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                if(i==0 && (j==0 || j==7)){
-                    gameBoard[i][j]=new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_ROOK);
+
+        for (int i = 0; i < 8; i++) {
+            IChess.ChessColor color = IChess.ChessColor.CLR_WHITE;
+            //gestion couleur
+            if (i < 2) {
+                color = IChess.ChessColor.CLR_BLACK;
+            } else if (i > 5) {
+                color = IChess.ChessColor.CLR_WHITE;
+            } else {
+                continue;
+            }
+            for (int j = 0; j < 8; j++) {
+                IChess.ChessType type = IChess.ChessType.TYP_PAWN;
+                if (i != 1 && i != 6) {
+                    switch (j) {
+                        case 0:
+                            type = IChess.ChessType.TYP_ROOK;
+
+                        case 7:
+                            type = IChess.ChessType.TYP_ROOK;
+                            break;
+                        case 1:
+                            type = IChess.ChessType.TYP_KNIGHT;
+
+                        case 6:
+                            type = IChess.ChessType.TYP_KNIGHT;
+                            break;
+                        case 2:
+                            type = IChess.ChessType.TYP_BISHOP;
+
+                        case 5:
+                            type = IChess.ChessType.TYP_BISHOP;
+                            break;
+                        case 3:
+                            type = IChess.ChessType.TYP_KING;
+                            break;
+
+                        case 4:
+                            type = IChess.ChessType.TYP_QUEEN;
+                            break;
+
+                    }
+
                 }
-                else if(i==7 && (j==0 || j==7)){
-                    gameBoard[i][j]=new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_ROOK);
-                }
-                else if(i==0 && (j==1 || j==6)){
-                    gameBoard[i][j]=new Piece(IChess.ChessColor.CLR_BLACK, IChess.ChessType.TYP_KNIGHT);
-                }
-                else if(i==7 && (j==1 || j==6)){
-                    gameBoard[i][j]=new Piece(IChess.ChessColor.CLR_WHITE, IChess.ChessType.TYP_KNIGHT);
-                }
+                gameBoard[i][j] = new Piece(color, type);
             }
         }
 
 
     }
 
-    public Piece getPiece(IChess.ChessPosition p){
+    public Piece getPiece(IChess.ChessPosition p) {
         return gameBoard[p.y][p.x];
     }
 
