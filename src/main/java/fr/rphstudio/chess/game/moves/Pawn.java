c2fda09f7bc4989a1105282cee8b92866b1e8483
Hugo Monnerie <hugo.monnerie@edu.itescia.fr>
2020-02-13 17:02:50 +0100

    prise en passant test

--- src/main/java/fr/rphstudio/chess/game/moves/Pawn.java
+++ src/main/java/fr/rphstudio/chess/game/moves/Pawn.java
@@ -27,33 +27,91 @@ public class Pawn implements IMove {
         }
 
         IChess.ChessPosition position = new IChess.ChessPosition(p.x, p.y + var);
-        if (Utils.isEmpty(position,gameBoard)) {
+        if (Utils.isEmpty(position, gameBoard)) {
             list.add(position);
         }
 
-        IChess.ChessPosition positionAtk = new IChess.ChessPosition(p.x-1, p.y +var);
-        IChess.ChessPosition positionAtk2 = new IChess.ChessPosition(p.x+1, p.y +var);
+        IChess.ChessPosition positionAtk = new IChess.ChessPosition(p.x - 1, p.y + var);
+        IChess.ChessPosition positionAtk2 = new IChess.ChessPosition(p.x + 1, p.y + var);
 
-        if(!Utils.isEmpty(positionAtk,gameBoard) && Utils.isEnemy(p,positionAtk,gameBoard)){
+        if (!Utils.isEmpty(positionAtk, gameBoard) && Utils.isEnemy(p, positionAtk, gameBoard)) {
             list.add(positionAtk);
         }
-        if(!Utils.isEmpty(positionAtk2,gameBoard) && Utils.isEnemy(p,positionAtk2,gameBoard)){
+        if (!Utils.isEmpty(positionAtk2, gameBoard) && Utils.isEnemy(p, positionAtk2, gameBoard)) {
             list.add(positionAtk2);
         }
 
+        //prise en passant
+        //pion blanc vas a gauche
+        if (p.y == 3) {
+            if (gameBoard.getPiece(p).getChessColor() == IChess.ChessColor.CLR_WHITE && gameBoard.getPiece(p).getChessType() == IChess.ChessType.TYP_PAWN) {
+                IChess.ChessPosition po1 = new IChess.ChessPosition(p.x - 1, p.y);
+                IChess.ChessPosition poDead1 = new IChess.ChessPosition(p.x , p.y+1);
 
-        if (!gameBoard.getPiece(p).asMoved()) {
-            var *= 2;
-            IChess.ChessPosition position2 = new IChess.ChessPosition(p.x, p.y + var);
-            if (Utils.isEmpty(position,gameBoard)) {
-                list.add(position2);
+                if (!Utils.isEmpty(po1, gameBoard) && gameBoard.getPiece(po1).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(po1).getChessType() == IChess.ChessType.TYP_PAWN) {
+                    list.add(positionAtk);
+                    if(!Utils.isEmpty(poDead1, gameBoard) && gameBoard.getPiece(poDead1).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(po1).getChessType() == IChess.ChessType.TYP_PAWN) {
+                        gameBoard.setPiece(poDead1 , null);
+                    }
+                }
+            }
+        }
+        //pion blanc vas a droite
+        if (p.y == 3) {
+            if (gameBoard.getPiece(p).getChessColor() == IChess.ChessColor.CLR_WHITE && gameBoard.getPiece(p).getChessType() == IChess.ChessType.TYP_PAWN) {
+                IChess.ChessPosition po2 = new IChess.ChessPosition(p.x + 1, p.y);
+                IChess.ChessPosition poDead2 = new IChess.ChessPosition(p.x , p.y+1);
+
+                if (!Utils.isEmpty(po2, gameBoard) && gameBoard.getPiece(po2).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(po2).getChessType() == IChess.ChessType.TYP_PAWN) {
+                    list.add(positionAtk2);
+                    if(!Utils.isEmpty(poDead2, gameBoard) && gameBoard.getPiece(poDead2).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(poDead2).getChessType() == IChess.ChessType.TYP_PAWN) {
+                        gameBoard.setPiece(poDead2 , null);
+                    }
+                }
+            }
+        }
+        //pion noir vas a gauche
+        if (p.y == 5) {
+            if (gameBoard.getPiece(p).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(p).getChessType() == IChess.ChessType.TYP_PAWN) {
+                IChess.ChessPosition po1 = new IChess.ChessPosition(p.x - 1, p.y);
+                IChess.ChessPosition poDead1 = new IChess.ChessPosition(p.x , p.y+1);
+
+                if (!Utils.isEmpty(po1, gameBoard) && gameBoard.getPiece(po1).getChessColor() == IChess.ChessColor.CLR_WHITE && gameBoard.getPiece(po1).getChessType() == IChess.ChessType.TYP_PAWN) {
+                    list.add(positionAtk2);
+                    if(!Utils.isEmpty(poDead1, gameBoard) && gameBoard.getPiece(poDead1).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(po1).getChessType() == IChess.ChessType.TYP_PAWN) {
+                        gameBoard.setPiece(poDead1 , null);
+                    }
+                }
+            }
+        }
+        //pion noir vas a droite
+        if (p.y == 5) {
+            if (gameBoard.getPiece(p).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(p).getChessType() == IChess.ChessType.TYP_PAWN) {
+                IChess.ChessPosition po2 = new IChess.ChessPosition(p.x + 1, p.y);
+                IChess.ChessPosition poDead2 = new IChess.ChessPosition(p.x , p.y+1);
+
+                if (!Utils.isEmpty(po2, gameBoard) && gameBoard.getPiece(po2).getChessColor() == IChess.ChessColor.CLR_WHITE && gameBoard.getPiece(po2).getChessType() == IChess.ChessType.TYP_PAWN) {
+                    list.add(positionAtk);
+                    if(!Utils.isEmpty(poDead2, gameBoard) && gameBoard.getPiece(poDead2).getChessColor() == IChess.ChessColor.CLR_BLACK && gameBoard.getPiece(poDead2).getChessType() == IChess.ChessType.TYP_PAWN) {
+                        gameBoard.setPiece(poDead2 , null);
+                    }
+
+                }
+            }
+        }
+
+            if (!gameBoard.getPiece(p).asMoved()) {
+                var *= 2;
+                IChess.ChessPosition position2 = new IChess.ChessPosition(p.x, p.y + var);
+                if (Utils.isEmpty(position, gameBoard)) {
+                    list.add(position2);
+                }
+
             }
 
+
+            return list;
         }
 
 
-        return list;
     }
-
-
-}
